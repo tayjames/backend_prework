b366467697a8b67ff19452b1f69c394f10f077b1
Tay <tayd93@hotmail.com>
2019-04-01 21:03:20 -0600

    Finalizes ex1.

--- day_1/ex1.rb
+++ day_1/ex1.rb
@@ -1,10 +1,11 @@
+
 puts "Hello World!"
-puts "Hello Again"
+puts "Hello again"
 puts "I like typing this."
-puts "This is fun."
+puts "This is very fun."
 puts "Yay! Printing."
 puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
-puts "I just want to be your lovergirl"
-#I just want to rock your wooooorld
-#is this how comments work???
+puts 'I "said" do not touch this. '
+puts "I know that it's hard to digest but realization is as good as it gets!"
+#use an octothorpe to write comments, or disable code but
+#beware when using them in strings bc they will actually print.
