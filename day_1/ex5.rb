0ef89fe1fe31eaebb0a1d1df450c5be5c8bc280a
Tay <tayd93@hotmail.com>
2019-04-01 22:10:57 -0600

    Finalizes ex5

--- day_1/ex5.rb
+++ day_1/ex5.rb
@@ -20,3 +20,20 @@ cm = height * 2.54
 kg = weight / 2.205
 
 puts "He's #{cm} centimeters tall and #{kg} kilograms heavy...that's not too heavy."
+
+my_name = "Tay J. DeHerrera"
+my_age = 25
+my_height = 69 # inches
+my_weight = 150 # lbs
+my_eyes = "Dark Brown"
+my_teeth = "White"
+my_hair = "Black"
+
+puts "Let's talk about #{my_name}."
+puts "They're #{my_height} inches tall."
+puts "They're #{my_weight} lbs heavy."
+puts "Actually that's not too heavy."
+puts "They've got #{my_eyes} eyes and #{my_hair} hair."
+puts "Their teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
