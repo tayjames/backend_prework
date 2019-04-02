0a0dccd2b1f4775b1d96655bc4490b6c1cf2f6b0
Tay <tayd93@hotmail.com>
2019-04-01 21:47:53 -0600

    Finalizes ex4.rb

--- day_1/ex4.rb
+++ day_1/ex4.rb
@@ -10,11 +10,10 @@ passengers = 90
 cars_not_driven = cars - drivers
 #assigning the variable cars_driven to the variable "drivers"
 cars_driven = drivers
-#assigning the variable carpoot_capacity equal to the operation cars_driven * space_in_a_car
+#assigning the variable carpool_capacity equal to the operation cars_driven * space_in_a_car
 carpool_capacity = cars_driven * space_in_a_car
 #assigning the variable average_passengers_per_car equal to the operation passengers / cars_driven
 average_passengers_per_car = passengers / cars_driven
-
 
 puts "There are #{cars} cars available."
 puts "There are only #{drivers} drivers available."
@@ -22,3 +21,7 @@ puts "There will be #{cars_not_driven} empty cars today"
 puts "We can transport #{carpool_capacity} people today."
 puts "We have #{passengers} to carpool today."
 puts "we need to put about #{average_passengers_per_car} in each car."
+
+# Remember that we use '_' to replace a space when naming variables.
+# I think it's necessary to use a flot for space_in_a_car bc it will give a better
+#indication of how many people will fit in the car w/out rounding. 
